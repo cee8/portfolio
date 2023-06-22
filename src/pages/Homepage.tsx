41a59b694fdc155a5dde1af7c6951f7602da4935
Chris Beaudoin <chrisbeaudoin.school@gmail.com>
2023-06-22 00:27:33 -0700

    Changed layout in order to include image, added grid to center content, changed font/font size, changed color.

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
 import Paper from '@material-ui/core/Paper'
 import Divider from '@material-ui/core/Divider'
-// import Chip from '@mui/material/Chip'
+import Chip from '@material-ui/core/Chip'
 import Button from '@material-ui/core/Button'
-// import AssignmentIcon from '@material-ui/icons/Assignment'
+import { initializeApp } from 'firebase/app'
 
 const useStyles = makeStyles(
   theme => ({
@@ -16,13 +16,14 @@ const useStyles = makeStyles(
       'header header'
       'leftpan rightpan'
       'bottomleftpan rightpan'
+      'footer footer'
       `,
-      height: '100vh',
-      width: '100vw',
+      gridTemplateColumns: 'auto 700px',
+      gridTemplateRows: '8% 1fr 23%',
+      width: 'auto',
+      minHeight: '100vh',
     },
-
     header: {
-      // height: '10vh',
       gridArea: 'header',
       backgroundColor: 'white',
       display: 'flex',
@@ -30,81 +31,171 @@ const useStyles = makeStyles(
       paddingRight: '3rem',
       flexDirection: 'row',
     },
-
+    smallText: {
+      fontFamily: 'Trebuchet MS',
+      fontSize: '23px',
+      fontWeight: 'normal',
+    },
+    mediumHighText: {
+      fontFamily: 'Trebuchet MS',
+      fontSize: '30px',
+      fontWeight: 'bold',
+      paddingTop: '30px',
+    },
     avatarContainer: {},
-
     rounded: {},
-
-    // chrisAvatar:
-
     title: {
       fontSize: theme.typography.h4.fontSize,
     },
-
     directory: {
       display: 'flex',
       justifyContent: 'space-evenly',
       padding: '10px',
     },
-
     directoryHome: {
       paddingInline: '10px',
     },
-
     directoryWorks: {
       paddingInline: '10px',
     },
-
     directoryProjects: {
       paddingInline: '10px',
     },
-
     directoryServices: {
       paddingInline: '10px',
     },
-
     chatBox: {},
-
-    introBox: {},
-
-    introChip: {},
-
-    introText: {},
-
-    subText: {},
-
-    hireMe: {},
-
-    projects: {},
-
-    clients: {},
-
-    finishedProjects: {},
-
-    contact: {},
-
+    topIntroLine: {
+      display: 'flex',
+      justifyContent: 'start',
+      height: 'auto',
+      alignItems: 'center',
+      fontFamily: 'Gill Sans (sans-serif)',
+      fontSize: 'xx-large',
+      letterSpacing: '0.1px',
+      whiteSpace: 'nowrap',
+    },
+    introBox: {
+      paddingInline: '15%',
+    },
+    largeChip: {
+      border: 'solid',
+      borderRadius: '50px',
+      height: '50px',
+      width: '135px',
+      fontFamily: 'Trebuchet MS',
+      fontSize: '40px',
+      fontWeight: 'bold',
+      backgroundColor: 'purple',
+      borderColor: 'purple',
+      color: 'white',
+    },
+    introText: {
+      paddingRight: '20px',
+    },
+    nameIntroLine: {
+      fontFamily: 'Gill Sans (sans-serif)',
+      fontSize: 'xx-large',
+    },
+    largeTextTop: {
+      width: '400px',
+      marginTop: '90px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      fontSize: '70px',
+      fontWeight: 'bold',
+      fontFamily: 'Gill Sans (sans-serif)',
+      maxHeight: '100px',
+    },
+    largeText: {
+      display: 'flex',
+      fontSize: '70px',
+      fontWeight: 'bold',
+      fontFamily: 'Gill Sans (sans-serif)',
+    },
+    subText: {
+      display: 'flex',
+      fontFamily: 'Trebuchet MS',
+      fontSize: '23px',
+      fontWeight: 'normal',
+      paddingBlock: '50px',
+    },
+    introBottomComponents: {
+      display: 'grid',
+      gridTemplateAreas: `
+        'hireprojects hireprojects logo    logo'
+        'clients      clients      logo    logo'
+        'finprojects  contact      contact contact'`,
+      gridTemplateColumns: 'auto auto auto auto',
+      gridTemplateRows: 'auto auto auto',
+      justifyItems: 'left',
+      alignItems: 'center',
+    },
+    hireAndProjectsButtons: {
+      gridArea: 'hireprojects',
+      display: 'flex',
+      justifyContent: 'space-between',
+      maxWidth: '350px',
+      minWidth: '325px',
+    },
+    hireMe: {
+      gridArea: 'hire',
+      backgroundColor: 'orange',
+      width: '150px',
+      height: '55px',
+      paddingRight: '20px',
+    },
+    projects: {
+      gridArea: 'projects',
+      width: '150px',
+      height: '55px',
+    },
+    mainLogo: {
+      gridArea: 'logo',
+      width: 'auto',
+      maxHeight: '100px',
+    },
+    clients: {
+      gridArea: 'clients',
+      display: 'flex',
+      flexDirection: 'column',
+      alignContent: 'start',
+    },
+    finishedProjects: {
+      gridArea: 'finprojects',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    finProjNum: {},
+    contact: {
+      gridArea: 'contact',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'start',
+    },
+    contactHead: {},
+    contactInfo: {},
     divider: {},
-
-    chrisPhoto: {},
-
-    footer: {},
+    footer: {
+      backgroundColor: '#212427',
+    },
     projStatA: {},
     projStatB: {},
     projStatC: {},
-
     content: {
       gridArea: 'rightpan',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
+      minHeight: 0,
+      overflow: 'hidden',
     },
-
     cardContentAbove: {
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'space-around',
     },
-
     project: {
       height: '80%',
       backgroundColor: 'white',
@@ -114,25 +205,13 @@ const useStyles = makeStyles(
       flexDirection: 'column',
       justifyContent: 'space-around',
     },
-
-    // projectTwo: {
-    //   backgroundColor: 'rgb(255, 172, 28)',
-    //   minHeight: theme.spacing(20),
-    //   margin: theme.spacing(3),
-    //   display: 'flex',
-    //   justifyContent: 'space-around',
-    // },
-
-    // projectThree: {
-    //   backgroundColor: 'rgb(255, 112, 28)',
-    //   minHeight: theme.spacing(20),
-    //   margin: theme.spacing(3),
-    //   display: 'flex',
-    //   justifyContent: 'space-around',
-    // },
-
-    contentImage: {},
-
+    leafPhoto: {
+      gridArea: 'logo',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      objectPosition: 'center',
+    },
     contentDescription: {},
   }),
   { name: 'Homepage' }
@@ -140,6 +219,18 @@ const useStyles = makeStyles(
 
 export const Homepage = () => {
   const classes = useStyles()
+
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBzDaI4n4GzETmNOk6Idxp7zZLIC5ero_Y',
+    authDomain: 'portfolio-num1.firebaseapp.com',
+    projectId: 'portfolio-num1',
+    storageBucket: 'portfolio-num1.appspot.com',
+    messagingSenderId: '880920820026',
+    appId: '1:880920820026:web:0a7474bf39f5913bd27803',
+  }
+
+  initializeApp(firebaseConfig)
+
   return (
     <div className={classes.root}>
       <div className={classes.header}>
@@ -156,16 +247,43 @@ export const Homepage = () => {
         <div className={classes.chatBox}>CHAT</div>
       </div>
       <div className={classes.introBox}>
-        {/* <Chip className={classes.introChip} label="Chip Filled" /> */}
-        <h1 className={classes.introText}>Hi! I am Chris Beaudoin</h1>
-        <h4 className={classes.subText}>This is subtext this ix subtext this is subtextds</h4>
-        <Button className={classes.hireMe} variant="contained">
-          Hire me
-        </Button>
-        <Button className={classes.projects}>Projects</Button>
-        <h2 className={classes.clients}>clients</h2>
-        <h2 className={classes.finishedProjects}>FinishedProjects</h2>
-        <h2 className={classes.contact}>contact</h2>
+        <div className={classes.topIntroLine}>
+          <div className={classes.largeTextTop}>
+            Hi! I am
+            <div className={classes.largeChip}>UI/UX</div>
+          </div>
+        </div>
+        <div className={classes.nameIntroLine}>
+          <div className={classes.largeText}>Chris Beaudoin</div>
+        </div>
+        <h3 className={classes.subText}>
+          Designing user interfaces for over 90 years as a product designer
+        </h3>
+        <div className={classes.introBottomComponents}>
+          <div className={classes.hireAndProjectsButtons}>
+            <Button className={classes.hireMe} variant="contained">
+              Hire me
+            </Button>
+            <Button className={classes.projects}>Projects</Button>
+          </div>
+          <img
+            className={classes.mainLogo}
+            src="https://www.nicepng.com/png/full/839-8393628_purple-leaf.png"
+            alt="leaf Photo"
+          />
+          <div className={classes.clients}>
+            <div className={classes.mediumHighText}>+84</div>
+            <div className={classes.smallText}>Clients on work worldwide</div>
+          </div>
+          <div className={classes.finishedProjects}>
+            <div className={classes.mediumHighText}>572</div>
+            <div className={classes.smallText}>Projects Done</div>
+          </div>
+          <div className={classes.contact}>
+            <div className={classes.mediumHighText}>Contact</div>
+            <div className={classes.smallText}>chrisbeaudoin.school@gmail.com</div>
+          </div>
+        </div>
       </div>
       <div className={classes.footer}>
         <div className={classes.projStatA}>A</div>
@@ -174,12 +292,14 @@ export const Homepage = () => {
         </div>
       </div>
       <div className={classes.content}>
-        <div className={classes.chrisPhoto}>
-        <div class="swiper-zoom-container">
-      <img id="fullwall" src="some link">
-    </div>
-        </div>
+        <img
+          className={classes.leafPhoto}
+          src="https://e0.pxfuel.com/wallpapers/975/533/desktop-wallpaper-paper-walls-neon-plants-iphone-phone-galaxy-cool-neon-phone-thumbnail.jpg"
+          alt="leaf Photo"
+        />
       </div>
     </div>
   )
 }
+
+export default Homepage
---
+++ src/pages/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzDaI4n4GzETmNOk6Idxp7zZLIC5ero_Y',
+  authDomain: 'portfolio-num1.firebaseapp.com',
+  projectId: 'portfolio-num1',
+  storageBucket: 'portfolio-num1.appspot.com',
+  messagingSenderId: '880920820026',
+  appId: '1:880920820026:web:0a7474bf39f5913bd27803',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
